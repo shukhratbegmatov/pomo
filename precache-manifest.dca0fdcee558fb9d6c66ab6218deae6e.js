2590e6c898c74657314b3c027c27a0065863cb69
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2021-08-26 16:06:39 +0500

    app

---
+++ precache-manifest.dca0fdcee558fb9d6c66ab6218deae6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7df1a3bf56142df075",
+    "url": "/css/app.1c17b6db.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f3cbeb3a0cdae560de359f4959aa6d1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e7df1a3bf56142df075",
+    "url": "/js/app.ee2c525c.js"
+  },
+  {
+    "revision": "11a05496de6693a1c9ba",
+    "url": "/js/chunk-vendors.a4d1c2c8.js"
+  },
+  {
+    "revision": "dda8a48a7a3138444674f6159ec4efba",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
